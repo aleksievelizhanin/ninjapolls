58b9b4c4702c028d9cd32b929e22084495354552
aleksievelizhanin <avelizhanin@yahoo.com>
2024-06-03 19:40:00 +0300

    Add Footer and Header components

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,21 @@
 <script>
-	export let name;
+    import Footer from "./components/Footer.svelte";
+	import Header from "./components/Header.svelte";
 </script>
 
+<Header></Header>
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempus est ac est vehicula lobortis. Proin porttitor vestibulum sollicitudin. Vivamus id bibendum dui. Cras aliquet velit nibh, in ornare est congue eget. Praesent mollis cursus justo, a iaculis lorem finibus at. Pellentesque tortor nisl, tincidunt eget ultrices tempus, auctor a massa. Aliquam massa ante, rhoncus ut leo eget, pulvinar consequat ante. Vivamus at ligula rhoncus quam gravida vulputate. Vestibulum nec scelerisque nibh. Sed rhoncus placerat elit, in pharetra neque dignissim eget. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent tristique consectetur pellentesque. Aliquam mollis odio vel interdum dignissim.
+
+		Praesent mi eros, porttitor eu ipsum id, venenatis tincidunt erat. Nam ac risus vitae est dapibus ornare commodo sit amet eros. Maecenas pretium mattis ex. Donec nec justo vehicula, sodales lectus sit amet, egestas nisi. Integer et congue nibh, ac blandit libero. Cras tempor sem a tincidunt placerat. Proin laoreet metus enim. Mauris a felis id nibh commodo euismod sit amet a elit.
+		
+		Cras posuere dui vel massa fermentum, sit amet feugiat justo cursus. Aenean sit amet fermentum enim. Aenean vel sapien nec ipsum pharetra egestas. Mauris vulputate sit amet justo eget euismod. Suspendisse varius nunc nec risus iaculis tincidunt. Nunc sagittis tempus cursus. Aliquam accumsan, ligula et placerat dignissim, metus metus aliquam nisi, in luctus neque lorem ac libero. Pellentesque luctus sapien vel purus elementum, vitae semper enim tempus. Phasellus in mi ac tellus elementum accumsan.</p>
 </main>
+<Footer></Footer>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	main{
+		max-width: 960px;
+		margin: 40px;
 	}
 </style>

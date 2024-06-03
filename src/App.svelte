80e2c091eda80d2d478536dc6cf129e0fac4f950
aleksievelizhanin <avelizhanin@yahoo.com>
2024-06-03 20:22:25 +0300

    Add shared component Tabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,26 @@
 <script>
     import Footer from "./components/Footer.svelte";
 	import Header from "./components/Header.svelte";
+    import Tabs from "./shared/Tabs.svelte";
+
+	let items = ['Polls', 'Add new one'];
+	let activeItem = 'Polls';
+	const changeTab = (e) => {
+		activeItem = e.detail;
+	}
 </script>
 
 <Header></Header>
-<main>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempus est ac est vehicula lobortis. Proin porttitor vestibulum sollicitudin. Vivamus id bibendum dui. Cras aliquet velit nibh, in ornare est congue eget. Praesent mollis cursus justo, a iaculis lorem finibus at. Pellentesque tortor nisl, tincidunt eget ultrices tempus, auctor a massa. Aliquam massa ante, rhoncus ut leo eget, pulvinar consequat ante. Vivamus at ligula rhoncus quam gravida vulputate. Vestibulum nec scelerisque nibh. Sed rhoncus placerat elit, in pharetra neque dignissim eget. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent tristique consectetur pellentesque. Aliquam mollis odio vel interdum dignissim.
 
-		Praesent mi eros, porttitor eu ipsum id, venenatis tincidunt erat. Nam ac risus vitae est dapibus ornare commodo sit amet eros. Maecenas pretium mattis ex. Donec nec justo vehicula, sodales lectus sit amet, egestas nisi. Integer et congue nibh, ac blandit libero. Cras tempor sem a tincidunt placerat. Proin laoreet metus enim. Mauris a felis id nibh commodo euismod sit amet a elit.
-		
-		Cras posuere dui vel massa fermentum, sit amet feugiat justo cursus. Aenean sit amet fermentum enim. Aenean vel sapien nec ipsum pharetra egestas. Mauris vulputate sit amet justo eget euismod. Suspendisse varius nunc nec risus iaculis tincidunt. Nunc sagittis tempus cursus. Aliquam accumsan, ligula et placerat dignissim, metus metus aliquam nisi, in luctus neque lorem ac libero. Pellentesque luctus sapien vel purus elementum, vitae semper enim tempus. Phasellus in mi ac tellus elementum accumsan.</p>
+<main>
+	<Tabs {items} {activeItem} on:tabChange={changeTab}></Tabs>
+	{#if activeItem === 'Polls'}
+		<div>Polls component will be here => </div>
+	{:else if activeItem === 'Add new one'}	
+		<div>Add new poll form is here => </div>
+	{/if}
 </main>
+
 <Footer></Footer>
 
 <style>

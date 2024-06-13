29382cb178a154443fc5a536aa863b1f9a2d771a
aleksievelizhanin <avelizhanin@yahoo.com>
2024-06-13 06:17:14 +0300

    Add Card for PollDetails

---
+++ src/shared/Card.svelte
@@ -0,0 +1,12 @@
+<div class="card">
+    <slot></slot>
+</div>
+
+<style>
+    .card {
+        background: white;
+        padding: 20px;
+        border-radius: 5px;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    }
+</style>
